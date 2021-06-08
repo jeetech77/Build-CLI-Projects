08d9618d3724ba720764bb65c13915f8be811b2d
Ranjeet Singh <44586579+jeetech77@users.noreply.github.com>
2021-06-08 22:50:56 +0530

    Finished : Max mark problem
    Solved another CLI problem which will return the Max and Average marks of the students.

---
+++ studentsMarks.js
@@ -0,0 +1,24 @@
+const readlineSync = require('readline-sync');
+var studentsDB = {
+    name: [],
+    totalMark: []
+}
+let numOfStudents = 5;
+for (let index = 0; index < numOfStudents; index++) {
+    studentsDB.name[index] = readlineSync.question(`Enter the name of ${index + 1} student...\n`);
+    let studentsmarks = [];
+    studentsmarks[index] = parseInt(readlineSync.question(`Enter the unit test marks of the ${index + 1} student...\n`));
+    studentsmarks[index + 1] = parseInt(readlineSync.question(`Enter the pre final marks of the ${index + 1} student...\n`));
+    studentsmarks[index + 2] = parseInt(readlineSync.question(`Enter the final of the ${index + 1} student...\n`));
+    console.log(typeof (studentsmarks));
+    studentsDB.totalMark[index] = parseInt(studentsmarks[index] + studentsmarks[index + 1] + studentsmarks[index + 2]);
+};
+console.table(studentsDB);
+let allStudentsMark = 0;
+studentsDB.totalMark.forEach(value => {
+    allStudentsMark += value;
+})
+console.log(`Average marks of the students is : ${allStudentsMark / numOfStudents}`);
+maxMark = Math.max(...studentsDB.totalMark);
+console.log(`Max mark of the student is : ${maxMark}`);
+console.log(`Name of the student who get maximum mark is : ${studentsDB.name[studentsDB.totalMark.indexOf(maxMark)]}`);
