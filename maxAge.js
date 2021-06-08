f191b7bf4578f8ff3f9026b7fb433ff85453e754
Ranjeet Singh <44586579+jeetech77@users.noreply.github.com>
2021-06-08 21:42:18 +0530

    Complted : Max Age exercise.
    Build/interview question.

---
+++ maxAge.js
@@ -0,0 +1,17 @@
+const personOne={
+    name:"Ram",
+    age:31,
+    yuga:"Treta"
+}
+const personTwo={
+    name:"Krishna",
+    age:28,
+}
+function maxAge(objOne,objTwo){
+    if (objOne.age>objTwo.age) {
+        return objOne.name; // Returning the name of the person with more age.
+    }else{
+        return objTwo.name;  // Returning the name of the person with more age.
+    }
+}
+console.log(maxAge(personOne,personTwo));
